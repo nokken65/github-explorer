c40498cf336bcc3f68cb2bedab98654524569e69
nokken65 <unknown65182@gmail.com>
2022-10-19 17:28:56 +0300

    add splitting by chunks

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,26 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
   ],
-  build: { sourcemap: true },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'effector-vendor': [
+            'effector',
+            'effector-react',
+            '@effector/reflect',
+          ],
+          'router-vendor': ['atomic-router', 'atomic-router-react', 'history'],
+          'nextui-vendor': ['@nextui-org/react', 'next-themes'],
+          'other-vendor': ['react-hotkeys-hook', 'myzod', 'dayjs'],
+          'api-vendor': ['@urql/core', 'graphql'],
+          'forms-vendor': ['react-hook-form'],
+        },
+      },
+    },
+  },
   server: {
     host: true,
     port: 3000,
